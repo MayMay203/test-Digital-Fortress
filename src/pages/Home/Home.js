e2d9e5667fdc7c31197383709f6d606d97933ea2
Nhung <hongnhung16052003@gmail.com>
2024-09-19 11:53:49 +0700

    build UI of the main content

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,21 @@
+import Button from '~/components/Button';
 import styles from './Home.module.scss'
 import classNames from 'classnames/bind';
+import Table from '~/components/Table';
 
 const cx = classNames.bind(styles)
 function Home() {
-    return (<div className={cx('wrapper')}>Home page</div> );
+    return (
+      <div className={cx('wrapper')}>
+        <h1 className={cx('heading')}>Dashboard</h1>
+          <Table/>
+        <div className={cx('pagination')}>
+          <Button className={cx('btn')}>Previous</Button>
+          <span className={cx('title')}>Page 1 of 10</span>
+          <Button className={cx('btn')}>Next</Button>
+        </div>
+      </div>
+    )
 }
 
 export default Home;
